3f34fb33610cddf7e52605292ca35d1a2b7fc576
KiddHsieh <hsieh.kidd@inventec.com>
2018-03-07 10:50:10 +0800

    Combine IPMI OEM(openpower and qualcomm)
    Based on openpower to add qualcomm patch.

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-This .so file is designed to support the OpenPOWER's BIOS OEM commands.
-Documentation can be found by contacting the OpenPOWER mailing list @ https://github.com/open-power/op-build
+This .so file is designed to support the ARM BIOS OEM commands.
 
 ## To Build
 ```
--- oemhandler.hpp
+++ oemhandler.hpp
@@ -1,6 +1,3 @@
-#ifndef __HOST_IPMI_OPENPOWEROEM_HANDLER_H__
-#define __HOST_IPMI_OPENPOWEROEM_HANDLER_H__
-
 #include <stdint.h>
 #include <host-ipmid/ipmid-api.h>
 
@@ -58,5 +55,3 @@ ipmi_ret_t ipmi_qdt_oem_set_boot_mode(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
 ipmi_ret_t ipmi_qdt_oem_get_boot_mode(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
                               ipmi_request_t request, ipmi_response_t response,
                               ipmi_data_len_t data_len, ipmi_context_t context);
-
-#endif
